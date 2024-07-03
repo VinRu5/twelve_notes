3050d44cb9606952e6521c79146027beb09b5707
Vincenzo Russo <vincenzorusso.inbox@gmail.com>
2024-07-03 14:10:51 +0200

    chore: initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:twelve_notes/firebase_options.dart';
 import 'package:twelve_notes/src/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }

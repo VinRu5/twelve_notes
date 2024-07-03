c0d2990bdae7d0fd8cbe0a7a7750842874b194dc
Vincenzo Russo <vincenzorusso.inbox@gmail.com>
2024-07-03 17:31:22 +0200

    chore: added dependencies and localization

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:twelve_notes/src/theme/mockup_widget.dart';
 import 'package:twelve_notes/src/theme/twelve_theme.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Twelve Notes',
+        title: AppLocalizations.of(context).appName,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
         theme: TwelveTheme.light,
         darkTheme: TwelveTheme.dark,
         home: MockupWidget(),

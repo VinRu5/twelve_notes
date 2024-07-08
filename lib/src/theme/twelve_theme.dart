103cca45f7b507e8c227681e41d6022b958c7798
Vincenzo Russo <vincenzorusso.inbox@gmail.com>
2024-07-08 12:15:19 +0200

    chore: update theme and dependency

--- lib/src/theme/twelve_theme.dart
+++ lib/src/theme/twelve_theme.dart
@@ -61,10 +61,9 @@ class TwelveTheme {
     onSecondary: TwelveColors.textLight,
     error: TwelveColors.error,
     onError: TwelveColors.textDark,
-    background: TwelveColors.bgLight,
-    onBackground: TwelveColors.textLight,
     surface: TwelveColors.surfaceLight,
     onSurface: TwelveColors.textLight,
+    surfaceContainerHighest: TwelveColors.surfaceLight,
     outline: TwelveColors.primary,
   );
   static const ColorScheme _colorSchemeDark = ColorScheme(
@@ -75,10 +74,9 @@ class TwelveTheme {
     onSecondary: TwelveColors.textDark,
     error: TwelveColors.error,
     onError: TwelveColors.textDark,
-    background: TwelveColors.bgDark,
-    onBackground: TwelveColors.textDark,
-    surface: TwelveColors.surfaceDark,
+    surface: TwelveColors.bgDark,
     onSurface: TwelveColors.textDark,
+    surfaceContainerHighest: TwelveColors.surfaceDark,
     outline: TwelveColors.primary,
   );
 
@@ -87,10 +85,12 @@ class TwelveTheme {
   );
 
   static const AppBarTheme _appBarThemeLight = AppBarTheme(
+    surfaceTintColor: TwelveColors.bgLight,
     color: TwelveColors.bgLight,
     centerTitle: true,
   );
   static const AppBarTheme _appBarThemeDark = AppBarTheme(
+    surfaceTintColor: TwelveColors.bgDark,
     color: TwelveColors.bgDark,
     centerTitle: true,
   );
@@ -202,10 +202,10 @@ class TwelveTheme {
   );
 
   static const SearchBarThemeData _searchBarThemeDataLight = SearchBarThemeData(
-    backgroundColor: MaterialStatePropertyAll(TwelveColors.surfaceLight),
-    shadowColor: MaterialStatePropertyAll(TwelveColors.primary),
-    surfaceTintColor: MaterialStatePropertyAll(Colors.white54),
-    shape: MaterialStatePropertyAll(
+    backgroundColor: WidgetStatePropertyAll(TwelveColors.surfaceLight),
+    shadowColor: WidgetStatePropertyAll(TwelveColors.primary),
+    surfaceTintColor: WidgetStatePropertyAll(Colors.white54),
+    shape: WidgetStatePropertyAll(
       RoundedRectangleBorder(
         borderRadius: BorderRadius.all(
           Radius.circular(24.0),
@@ -214,10 +214,10 @@ class TwelveTheme {
     ),
   );
   static const SearchBarThemeData _searchBarThemeDataDark = SearchBarThemeData(
-    backgroundColor: MaterialStatePropertyAll(TwelveColors.surfaceDark),
-    shadowColor: MaterialStatePropertyAll(TwelveColors.primary),
-    surfaceTintColor: MaterialStatePropertyAll(TwelveColors.surfaceLight),
-    shape: MaterialStatePropertyAll(
+    backgroundColor: WidgetStatePropertyAll(TwelveColors.surfaceDark),
+    shadowColor: WidgetStatePropertyAll(TwelveColors.primary),
+    surfaceTintColor: WidgetStatePropertyAll(TwelveColors.surfaceLight),
+    shape: WidgetStatePropertyAll(
       RoundedRectangleBorder(
         borderRadius: BorderRadius.all(
           Radius.circular(24.0),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,12 +9,16 @@ import firebase_auth
 import firebase_core
 import firebase_storage
 import flutter_secure_storage_macos
+import google_sign_in_ios
 import path_provider_foundation
+import sign_in_with_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
 }
